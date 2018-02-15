cf9662e41738502bb3d07568d81485f75a320fa0
Patrick Mexal <patrickmexal@gmail.com>
2018-02-15 14:18:24 -0700

    update ten

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured);
+VALUES ("Blue Cheese Bacon Burger", false);
+VALUES ("New Mexico Green Chile Burger", true);
+VALUES ("Kitchen Sink Burger w/ Fried Egg", false);
+VALUES ("Teriyaki Burger w/ Ponzu", false);
